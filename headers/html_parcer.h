0ff9510ba4b7d88d5b599cd49e79e3235cdd8d4d
MaximMGG <maxhrunenko@gmail.com>
2024-08-04 19:52:35 +0300

    hp_parser.c

--- headers/html_parcer.h
+++ headers/html_parcer.h
@@ -2,12 +2,32 @@
 #define HTML_PARSER_H
 #include <cstdext/core.h>
 #include <cstdext/estring.h>
+#include <cstdext/arraylist.h>
+
+#define NONULL(...) __attribute__((__nonnull__(__VA_ARGS__)))
+
+typedef struct {
+    char *teg;
+    char *teg_class;
+    char *teg_class_param;
+
+    u32 skip;
+} hp_teg;
+
+typedef struct {
+    char *request;
+    Arraylist *param;
+
+    char *content;
+} hp_request;
 
 typedef struct {
     str html_content;
     u32 content_len;
 
     str request;
-} htmlp;
+} hp_html;
+
+char *hp_get_html_content(const char *request, const char *url) NONULL(1, 2);
 
 #endif //HTML_PARSER_H
--- src/html_parcer.c
+++ src/html_parcer.c
@@ -1,4 +1,11 @@
 #include "../headers/html_parcer.h"
+#include "../headers/hpnet.h"
 
 
+char *hp_get_html_content(const char *request, const char *url) {
+    char *html = hpnet_get_html(url);
+
+
+    return null;
+}
 
